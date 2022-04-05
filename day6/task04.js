664d9fdb8df7882bcefda5a894f34570a13304b5
Giovanni <giovanni@irt.bj>
2022-04-05 10:13:15 +0100

    day6 task4

--- day6/task04.js
+++ day6/task04.js
@@ -1,5 +1,31 @@
 module.exports = {
-    fizzBuzz: function(str){
-        
+    fizzBuzz: function(num){
+        let display = '';
+        if(!isNaN(num)){
+
+            if(num <= 0){
+                display = '';
+            }
+            
+            for(let i = 1; i <= num; i++){
+
+                if(i == 1){
+                    display = '' + 1;
+                }else{
+                    if((i % 3) == 0 && (i % 5) == 0){
+                        display += ', FizzBuzz';
+                    }else if((i % 3) == 0){
+                        display += ', Fizz';  
+                    }else if((i % 5) == 0){
+                        display += ', Buzz';  
+                    }else{
+                        display += ', ' + i;
+                    }
+                }
+                
+            }
+
+        }
+        console.log(display);
     }
 }
