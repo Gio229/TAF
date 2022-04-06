f16dca4bec2c79f3b2b6630210b7b841f288ef04
Giovanni <giovanni@irt.bj>
2022-04-06 13:02:44 +0100

    day8 task1

---
+++ day8/task01.php
@@ -0,0 +1,14 @@
+<?php
+
+function discover_type ( int $age , string $name , float $gpa , bool $isStudent ){
+
+    if($isStudent){
+        echo "Hello my name is $name , I'm $age years old . I'm a student at Epitech with a GPA of $gpa.\n";
+    }else{
+        echo "Hello my name is $name , I'm $age years old . I'm not a student.\n";
+    }
+
+}
+
+discover_type(21, "Maria", 3.53, true);
+discover_type(52, "Jules", 0, false);
