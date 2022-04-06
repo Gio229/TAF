a32a2078e8b4850743712ad6e63150737635369c
Giovanni <giovanni@irt.bj>
2022-04-06 08:14:31 +0100

    day6 fix bug task6

--- day6/task06.js
+++ day6/task06.js
@@ -1,23 +1,23 @@
 module.exports = {
     objectsDeeplyEqual: function(cmp1, cmp2){
             
-        if(cmp1 !== cmp2){
+        if(cmp1 === null && cmp2 === null){
 
-            if(cmp1 instanceof Object && cmp2 instanceof Object){
-            
-                for (key in cmp1) {
-      
-                    if (cmp1[key] !== cmp2[key]) {
-                        return objectsDeeplyEqual(cmp1[key], cmp2[key]);
-                    }
-                    
+        }else if(cmp1 instanceof Object && cmp2 instanceof Object){
+            for (key in cmp1) {
+  
+                if (cmp1[key] !== cmp2[key]) {
+                    return objectsDeeplyEqual(cmp1[key], cmp2[key]);
                 }
-
-            }else{
-                return false;
+                
             }
+        }else if(cmp1 === cmp2){
+            //nothing
+        }else{
+            return false;
         }
 
-        return true;
+        return true
+
     }
 }
